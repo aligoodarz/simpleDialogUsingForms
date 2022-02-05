7ccf315f1ad2317d48b7d3d8c6aec1fb2b65f45a
aligoodarz <72888089+aligoodarz@users.noreply.github.com>
2022-02-05 16:20:36 -0500

    Added quit action
    Added quit action to the menuBar for a graceful exit.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include <QToolBar>
+#include <QApplication>
+#include <QMenu>
+#include <QMenuBar>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow{parent}
 {
-    QToolBar *toolbar = addToolBar("main toolbar");
-    toolbar->addAction("New File");
-    toolbar->addAction("Open File");
-    toolbar->addSeparator();
+//    QToolBar *toolbar = addToolBar("main toolbar");
+//    toolbar->addAction("New File");
+//    toolbar->addAction("Open File");
+//    toolbar->addSeparator();
+
+    auto* quit = new QAction ("&Quit",this);
+    QMenu *file = menuBar()->addMenu("&File");
+    file->addAction(quit);
+    connect (quit, &QAction::triggered, qApp, QApplication::quit);
+
+//    auto* save = new QAction()
 }
